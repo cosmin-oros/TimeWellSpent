c927fb537c6abb28b412cc0a4fe15025691ec461
cosmin-oros <oroscosmin09@gmail.com>
2023-01-13 18:31:10 +0200

    goals init

--- lib/goals_page.dart
+++ lib/goals_page.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class GoalsPage extends StatelessWidget {
+class GoalsPage extends StatefulWidget {
   const GoalsPage({super.key});
+
+  @override
+  _GoalsPageState createState() => _GoalsPageState();
+}
+
+class _GoalsPageState extends State<GoalsPage> {
+  List<String> goals = [];
+  List<bool> completedGoals = [];
+
+  void _deleteGoal(int index) {
+    setState(() {
+      goals.removeAt(index);
+      completedGoals.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +24,43 @@ class GoalsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Goals'),
       ),
-      body: const Center(
-        child: Text('This is the goals page'),
+      body: Column(
+        children: <Widget>[
+      Expanded(
+      child: ListView.builder(
+        itemCount: goals.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(goals[index]),
+            trailing: Checkbox(
+              value: completedGoals[index],
+              onChanged: (bool? value) {
+                setState(() {
+                  completedGoals[index] = value ?? false;
+                });
+              },
+            ),
+            onLongPress: () => _deleteGoal(index),
+          );
+        },
+      ),
+    ),
+    Container(
+      color: Colors.grey[200],
+      child: TextField(
+        decoration: const InputDecoration(
+          hintText: 'Add new goal',
+          border: InputBorder.none,
+        ),
+        onSubmitted: (String value) {
+          setState(() {
+            goals.add(value);
+            completedGoals.add(false);
+          });
+        },
+      ),
+    ),
+        ],
       ),
     );
   }
