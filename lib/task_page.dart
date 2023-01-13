68fd22ee278f52f0a2ba03c071f5d15903c269ac
cosmin-oros <oroscosmin09@gmail.com>
2023-01-13 18:08:06 +0200

    tasks init

--- lib/task_page.dart
+++ lib/task_page.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class TaskPage extends StatelessWidget {
+class TaskPage extends StatefulWidget {
   const TaskPage({super.key});
+
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  List<String> tasks = [];
+  List<bool> completedTasks = [];
+
+  void _deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+      completedTasks.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +24,46 @@ class TaskPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tasks'),
       ),
-      body: const Center(
-        child: Text('This is the task page'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(tasks[index]),
+                  trailing: Checkbox(
+                    value: completedTasks[index],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        completedTasks[index] = value ?? false;
+                      });
+                    },
+                  ),
+                  onLongPress: () => _deleteTask(index),
+                );
+              },
+            ),
+          ),
+          Container(
+            color: Colors.grey[200],
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Add new task',
+                border: InputBorder.none,
+              ),
+              onSubmitted: (String value) {
+                setState(() {
+                  tasks.add(value);
+                  completedTasks.add(false);
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
